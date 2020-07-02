ac467dbf9a2075b3c06a06f02d7873957c8f8cd4
Fortschrittarier <fortschrittarier@protonmail.com>
2020-07-02 15:22:42 +0200

    Logging on screen and and in file simultaniously

--- include/sampleslogger.hpp
+++ include/sampleslogger.hpp
@@ -12,29 +12,55 @@ protected:
     std::map<std::string, Sample> samples_;
     Timestamp program_start_;
 public:
-    SamplesLogger(Timestamp program_start) :
-        program_start_(program_start) {
+    SamplesLogger( Timestamp program_start ) :
+        program_start_( program_start ) {
     }
 
-    virtual void log() 
+    virtual void duo( std::ofstream& ostream ) 
     {
+        std::string log = formatted();
+        screen( log );
+        file( ostream, log );
+    }
+
+    virtual void file( std::ofstream& ofs, std::string& s ) 
+    {
+        ofs << s;
+    }
+
+    virtual void screen( std::string& s ) 
+    {
+        std::cout << s;
+    }
+
+    virtual void screen() 
+    {
+        std::cout << formatted();
+    }
+
+    virtual std::string formatted() {
+        std::stringstream srm;
         std::map<std::string, Sample> samples;
-        unqueue(samples);
-        for (auto const& [producer, spl] : samples) {
+
+        srm  << std::setprecision(5);
+
+        unqueue( samples );
+        for( auto const& [producer, spl] : samples ) {
             std::chrono::duration<double> elapsed_seconds = spl.getTimestamp() - program_start_;
-            std::cout << "[" << std::setw(4) << elapsed_seconds.count() << "s] '" << producer << "': " << spl.getValue() << std::endl;
+            srm << "[" << elapsed_seconds.count() << "s] '" << producer << "': " << spl.getValue() << std::endl;
         }
+        return srm.str();
     }
 
-    virtual void queue(std::string producer, Sample spl) 
+    virtual void queue( std::string producer, Sample spl ) 
     {
-        const std::lock_guard<std::mutex> lock(mx_);
-        samples_.insert({producer, spl});
+        const std::lock_guard<std::mutex> lock( mx_ );
+        samples_.insert( {producer, spl} );
     }
 
-    virtual void unqueue(std::map<std::string, Sample>& samples_copy) 
+    virtual void unqueue( std::map<std::string, Sample>& samples_copy ) 
     {
-        const std::lock_guard<std::mutex> lock(mx_);
+        const std::lock_guard<std::mutex> lock( mx_ );
         samples_copy = samples_;
         samples_.clear();
     }
--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <fstream>
 
 #include "include/sensor.hpp"
 #include "include/configparser.hpp"
@@ -5,62 +6,53 @@
 
 int main() {
 
+    // Save program start time
     using namespace std::chrono_literals;
     Timestamp program_start = std::chrono::system_clock::now();
 
+    // Set precision for floating point numbers on screen
     constexpr int precision = 4;
     constexpr int width = precision + 4;
     std::cout << std::fixed << std::setprecision(precision);
 
+    // Set interval for continous logging
     std::chrono::duration<int, std::milli> interval;
     interval = std::chrono::duration<int, std::milli>(100);
 
-/***                                                                       ***/
+    // Define config file and parse it
     std::string filePath = "/home/xif/dev/deepup/yaml/sensor_config.yml";
     ConfigParser parser(filePath);
 
+    // Create logger and empty sensor hub
     std::shared_ptr<SamplesLogger> logger = std::make_shared<SamplesLogger>(program_start);
     SensorHub hub(logger);
 
+    // Fill sensor hub with sensors
     for (SensorConfig cfg : parser.sensors()) {
         hub.createSensor(cfg);
     }
 
-    for (std::shared_ptr<Sensor> snr : hub.sensors()) {
-        std::cout << snr->getName() << std::endl;
-    }
-        std::cout << std::endl;
-
-
+    // Start all sensors
     hub.start();
-    
-    for (int i = 10000; i > 0; i--) {
+
+    // Define log file and open it
+    std::ofstream logfile_stream;
+    logfile_stream.open("sensor_log.txt");
+
+    // Run 100*interval ms long
+    for (int i = 100; i > 0; i--) {
         std::this_thread::sleep_for(interval);
-        logger->log();
-        // std::cout << t1.sense_blocking() << std::endl;
+        logger->duo( logfile_stream );
     }
 
+    // Stop all sensors
     hub.stop();
-/***                                                                        ***/
 
-    // NonBlockingSensor nb("nb 1", 1000);
-    // BlockingSensor b(" b 1", 3500);
-    // std::this_thread::sleep_for(1s);
+    // Flush remaining log data
+    logger->duo( logfile_stream );
 
-    // nb.start();
-    // b.start();
-    // std::this_thread::sleep_for(1010ms);
+    // Close the log file
+    logfile_stream.close();
     
-    // auto s = nb.sense();
-    // std::chrono::duration<double> elapsed_seconds = s.getTimestamp() - program_start;
-    // std::cout << "[" << std::setw(width) << elapsed_seconds.count() << "s] '" << nb.getName() << "': " << s.getValue() << std::endl;
-
-    // s = b.sense();
-    // elapsed_seconds = s.getTimestamp() - program_start;
-    // std::cout << "[" << std::setw(width) << elapsed_seconds.count() << "s] '" << b.getName() << "': " << s.getValue() << std::endl;
-
-    // b.stop();
-    // nb.stop();
-
     return 0;
 }
